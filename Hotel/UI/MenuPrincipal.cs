81479c966e3e5807df4c1f1165db99cd2606d620
Eduardo Arias <171630chuchi@gmail.com>
2022-05-21 03:09:56 -0400

    cambios en el menu dinamico

--- Hotel/UI/MenuPrincipal.cs
+++ Hotel/UI/MenuPrincipal.cs
@@ -36,14 +36,17 @@ namespace Hotel.UI
             leftborderBtn = new Panel();
             leftborderBtn.Size = new Size(7, 60);
             PanelMenu.Controls.Add(leftborderBtn);
+            this.DoubleBuffered = true;
+            this.MaximizedBounds = Screen.FromHandle(this.Handle).WorkingArea;
+
             
 
         }
 
         private struct RGBcolors
         {
-            public static Color color1 = Color.FromArgb(255, 255, 255);
-            public static Color color2 = Color.FromArgb(217, 124, 216);
+            public static Color color1 = Color.FromArgb(172, 126, 241);
+            public static Color color2 = Color.FromArgb(249, 118, 176);
             public static Color color3 = Color.FromArgb(253, 138, 114);
             public static Color color4 = Color.FromArgb(95,   77, 221);
           
@@ -58,9 +61,8 @@ namespace Hotel.UI
                 disableButton();
 
                 //boton
-
                 currentBtn = (IconButton)senderBtn;
-                currentBtn.BackColor = color;
+                currentBtn.BackColor = Color.AliceBlue;
                 currentBtn.ForeColor = color;
                 currentBtn.TextAlign = ContentAlignment.MiddleCenter;
                 currentBtn.IconColor = color;
@@ -72,8 +74,11 @@ namespace Hotel.UI
                 leftborderBtn.BackColor = color;    
                 leftborderBtn.Location = new Point(0, currentBtn.Location.Y);
                 leftborderBtn.Visible = true;
-                leftborderBtn.BringToFront();  
+                leftborderBtn.BringToFront();
 
+                //icono de inicio
+                iconhome.IconChar = currentBtn.IconChar;
+                iconhome.IconColor = color;
 
             }
 
@@ -83,25 +88,16 @@ namespace Hotel.UI
           if (currentBtn != null)
             {
                 currentBtn.BackColor = Color.FromArgb(2, 121, 186);
-                currentBtn.ForeColor = Color.Black;
+                currentBtn.ForeColor = Color.White;
                 currentBtn.TextAlign = ContentAlignment.MiddleLeft;
-                currentBtn.IconColor = Color.Black;
+                currentBtn.IconColor = Color.White;
                 currentBtn.TextImageRelation = TextImageRelation.ImageBeforeText;
                 currentBtn.ImageAlign = ContentAlignment.MiddleLeft;
-
-
-
             }
-
         }
-
-
-
-
 
         private void CustomComponentes()
         {
-
             for (int i = 0; i < MyIconButton.Count(); i++)
             {
                 MyIconButton[i] = new IconButton();
@@ -131,46 +127,57 @@ namespace Hotel.UI
 
         }
 
-
-
         private void MenuPrincipal_Load(object sender, EventArgs e)
         {
-
         }
-
         private void iconButton1_Click(object sender, EventArgs e)
         {
             CustomComponentes();
         }
-
         private void BtHotel_Click(object sender, EventArgs e)
         {
             ActivateButton(sender, RGBcolors.color1 );
-
         }
-
         private void BtAdministracion_Click(object sender, EventArgs e)
         {
             ActivateButton(sender, RGBcolors.color2);
         }
-
         private void iconButton3_Click(object sender, EventArgs e)
         {
             ActivateButton(sender, RGBcolors.color3);
         }
-
         private void iconButton4_Click(object sender, EventArgs e)
         {
             ActivateButton(sender, RGBcolors.color4);
         }
-
         private void pictureBox1_Click(object sender, EventArgs e)
         { 
+        }
+        private void PanelMenu_Paint(object sender, PaintEventArgs e)
+        {
+        }
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+            reset();
+        }
+        private void reset()
+        {
+            disableButton();
+            leftborderBtn.Visible = false;
+            iconhome.IconChar = currentBtn.IconChar;
+            iconhome.IconColor = Color.AliceBlue;   
+        }
+
+       
+
+
+        private void PNSuperior_Paint(object sender, MouseEventArgs e)
+
+        {
            
 
+
+
         }
-        
-
-
     }
 }
